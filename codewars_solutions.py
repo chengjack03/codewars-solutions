88e704f7df54227ba04234102394edb13c2c1430
Chun Cheng <cheng.jack@gmail.com>
2026-01-13 21:43:44 -0500

    Add Codewars Solutions Assignment

---
+++ codewars_solutions.py
@@ -0,0 +1,20 @@
+# Solution: Even or Odd
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+# Solution: Convert a Number to a String
+def number_to_string(num):
+    return str(num)
+
+
+# Solution: Remove String Spaces
+def no_space(x):
+    return x.replace(" ", "")
+
+# OR, if you chose Vowel Count
+def get_count(string):
+    return sum(1 for char in string if char in 'aeiouAEIOU')
